e664a8a1eb4e636a84f98e257cf83045b563b468
Luz <99538276+Luz-GM@users.noreply.github.com>
2022-08-24 09:10:35 -0300

    Configuracion back end

--- Portfolio_Garcia_Marquez_Maria_Luz/backEnd/src/main/java/com/portfolio/mlgm/controller/PersonaController.java
+++ Portfolio_Garcia_Marquez_Maria_Luz/backEnd/src/main/java/com/portfolio/mlgm/controller/PersonaController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,5 +34,17 @@ public class PersonaController {
              return "La persona fue eliminada correctamente";      
     }
     
-    
+    @PutMapping("/personas/editar/(id)")
+    public Persona editPersona(@PathVariable Long id,
+                               @RequestParam("nombre") String nuevoNombre,
+                               @RequestParam("apellido") String nuevoApellido,
+                               @RequestParam("img") String nuevoImg){
+        Persona persona = ipersonaService.findPersona(id);
+        persona.setNombre(nuevoNombre);
+        persona.setApellido(nuevoApellido);
+        persona.setImg(nuevoImg);
+        
+        ipersonaService.savePersona(persona);
+        return persona; 
+    }    
 }
