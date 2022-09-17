9a61b91ebdaefe893340bb88b4d6125b988b4eb5
Luz <99538276+Luz-GM@users.noreply.github.com>
2022-09-17 08:07:23 -0300

    Configuración login

--- Portfolio_Garcia_Marquez_Maria_Luz/frontEnd/src/app/app.module.ts
+++ Portfolio_Garcia_Marquez_Maria_Luz/frontEnd/src/app/app.module.ts
@@ -16,6 +16,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -35,10 +37,11 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     NgCircleProgressModule.forRoot({}),
     HttpClientModule,
   ],
-  providers: [],
+  providers: [interceptorProvider],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
