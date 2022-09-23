3325a866e12f84947a68f40b200778f8b639af49
Luz <99538276+Luz-GM@users.noreply.github.com>
2022-09-23 11:51:58 -0300

    Configuracion de Educacion (new & edit) Front end

--- Portfolio_Garcia_Marquez_Maria_Luz/frontEnd/src/app/app.module.ts
+++ Portfolio_Garcia_Marquez_Maria_Luz/frontEnd/src/app/app.module.ts
@@ -21,6 +21,8 @@ import { interceptorProvider } from './service/interceptor-service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NewEducacionComponent } from './components/educacion/new-educacion.component';
+import { EditEducacionComponent } from './components/educacion/edit-educacion.component';
 
 @NgModule({
   declarations: [
@@ -38,6 +40,8 @@ import { EditExperienciaComponent } from './components/experiencia/edit-experien
     LoginComponent,
     NewExperienciaComponent,
     EditExperienciaComponent,
+    NewEducacionComponent,
+    EditEducacionComponent,
   ],
   imports: [
     BrowserModule,
